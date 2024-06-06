f6e119da1931f9f6b6980a8839cd0e9fe5e41384
arulgundam <arul.gundam@gmail.com>
2024-06-05 22:35:32 -0400

    Fixed color by result toggle style. Now need to reformat layout of filter buttons.

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,6 @@ fetch('/data')
     .then(data => {
         console.log(data);
 
-        // Color mapping for teams
         const teamColors = {
             "Chennai Super Kings": "#fdc204",
             "Deccan Chargers": "#1a4667",
@@ -22,11 +21,14 @@ fetch('/data')
             "Sunrisers Hyderabad": "#fba81a"
         };
 
-        const assignColor = (team) => {
-            return teamColors[team] || 'rgb(30, 136, 229)'; // Default to blue if color not found
+        const assignColor = (team, filterMode, selectedTeam) => {
+            if (filterMode && selectedTeam) {
+                return team === selectedTeam ? 'green' : 'red';
+            } else {
+                return team ? teamColors[team] || 'rgb(30, 136, 229)' : 'rgb(30, 136, 229)';
+            }
         };
 
-        // Filter out null values and format seasons
         const seasons = [...new Set(data.map(d => d.season).filter(season => season !== null))]
             .map(season => parseInt(season))
             .sort((a, b) => a - b);
@@ -36,8 +38,7 @@ fetch('/data')
         const seasonFilter = document.getElementById('seasonFilter');
         const teamFilter = document.getElementById('teamFilter');
         const resetFiltersBtn = document.getElementById('resetFilters');
-        const colorModeSwitch = document.getElementById('flexSwitchCheckChecked');
-        const colorModeSwitchContainer = document.getElementById('colorModeSwitchContainer');
+        const colorSwitch = document.getElementById('flexSwitchCheckChecked');
 
         seasons.forEach(season => {
             seasonFilter.innerHTML += `<option value="${season}">${season}</option>`;
@@ -47,11 +48,12 @@ fetch('/data')
             teamFilter.innerHTML += `<option value="${team}">${team}</option>`;
         });
 
-        // Function to apply filters and update the plot
         const applyFilters = () => {
             const selectedSeason = seasonFilter.value;
             const selectedTeam = teamFilter.value;
-            const colorByTeamWin = colorModeSwitch.checked;
+            const filterMode = colorSwitch.checked;
+
+            colorSwitch.disabled = !selectedTeam;
 
             const filteredData = data.filter(d =>
                 (selectedSeason === "" || parseInt(d.season) === parseInt(selectedSeason)) &&
@@ -60,76 +62,91 @@ fetch('/data')
 
             const updatedFirstInningsScores = filteredData.map(d => parseInt(d.first_innings_score));
             const updatedSecondInningsScores = filteredData.map(d => parseInt(d.second_innings_score));
-
-            const updatedColors = filteredData.map(d => {
-                if (selectedTeam === "") {
-                    return assignColor();
-                } else if (colorByTeamWin) {
-                    return d.winner === selectedTeam ? 'green' : 'red';
-                } else {
-                    return assignColor(d.winner);
-                }
-            });
+            const updatedColors = filteredData.map(d => assignColor(d.winner, filterMode, selectedTeam));
 
             const updatedTrace = {
                 x: updatedFirstInningsScores,
                 y: updatedSecondInningsScores,
                 mode: 'markers',
                 marker: {
-                    size: 8,
+                    size: 12,
                     color: updatedColors,
                     symbol: 'square',
                     opacity: 0.8
                 },
                 type: 'scatter',
-                text: filteredData.map(d => `<i>${d.description}</i><br><span>${d.name} | <b>${d.result}</b> | 1st: ${d.first_innings_score}, 2nd: ${d.second_innings_score}</span>`),
+                text: filteredData.map(d => `<i>${d.description}</i><br><span>${d.name} | <b>${d.result}</b> | 1st: ${d.first_innings_score}, 2nd: ${d.second_innings_score}<span>`),
+                hoverinfo: 'text',
                 hoverlabel: {
                     bgcolor: 'white',
                     bordercolor: 'black',
                     font: { size: 12, color: 'black' },
                     namelength: 0
                 },
-                hovertemplate: '%{text}' // Display hover text
-            };
-
-            const layout = {
-                title: 'IPL Scorigami',
-                xaxis: {
-                    title: 'First Innings Score',
-                    range: 'auto'
-                },
-                yaxis: {
-                    title: 'Second Innings Score',
-                    range: 'auto'
-                },
-                hovermode: 'closest',
-                height: 600,
-                dragmode: false,
-                selectdirection: 'none'
+                hovertemplate: '%{text}'
             };
 
             Plotly.react('plot', [updatedTrace], layout);
-
-            // Show/hide the color mode switch based on team filter
-            if (selectedTeam !== "") {
-                colorModeSwitchContainer.style.display = 'block';
-            } else {
-                colorModeSwitchContainer.style.display = 'none';
-            }
         };
 
-        // Initial plot
-        applyFilters();
-
-        // Event listeners for filters and switch
-        seasonFilter.addEventListener('change', applyFilters);
-        teamFilter.addEventListener('change', applyFilters);
-        colorModeSwitch.addEventListener('change', applyFilters);
-        resetFiltersBtn.addEventListener('click', () => {
+        const resetFilters = () => {
             seasonFilter.value = '';
             teamFilter.value = '';
-            colorModeSwitch.checked = false;
+            colorSwitch.checked = false;
+            colorSwitch.disabled = true;
             applyFilters();
-        });
-    })
-    .catch(error => console.error('Error fetching data:', error));
+        };
+
+        const firstInningsScores = data.map(d => parseInt(d.first_innings_score));
+        const secondInningsScores = data.map(d => parseInt(d.second_innings_score));
+
+        const trace = {
+            x: firstInningsScores,
+            y: secondInningsScores,
+            mode: 'markers',
+            marker: {
+                size: 8,
+                color: 'rgb(30, 136, 229)',
+                symbol: 'square',
+                line: {
+                    color: 'rgb(30, 136, 229)',
+                    width: 0.5
+                },
+                opacity: 0.8
+            },
+            type: 'scatter',
+            text: data.map(d => `<i>${d.description}</i><br><span>${d.name} | <b>${d.result}</b> | 1st: ${d.first_innings_score}, 2nd: ${d.second_innings_score}<span>`),
+            hoverlabel: {
+                bgcolor: 'white',
+                bordercolor: 'black',
+                font: { size: 12, color: 'black' },
+                namelength: 0
+            },
+            hovertemplate: '%{text}'
+        };
+
+        const layout = {
+            title: 'IPL Scorigami',
+            xaxis: {
+                title: 'First Innings Score',
+                range: [40, 300],
+                autorange: true
+            },
+            yaxis: {
+                title: 'Second Innings Score',
+                range: [40, 300],
+                autorange: true
+            },
+            hovermode: 'closest',
+            height: 600,
+            dragmode: false,
+            selectdirection: 'none'
+        };
+
+        Plotly.newPlot('plot', [trace], layout);
+
+        seasonFilter.addEventListener('change', applyFilters);
+        teamFilter.addEventListener('change', applyFilters);
+        colorSwitch.addEventListener('change', applyFilters);
+        resetFiltersBtn.addEventListener('click', resetFilters);
+    });
